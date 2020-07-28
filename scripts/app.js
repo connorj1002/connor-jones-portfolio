16057298c8eba54ba06674d5263479bf5e0186c1
connorj1002 <connorj1002@gmail.com>
2020-07-27 19:12:54 -0600

    adds styling to home snippets

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,25 @@
-$("#underscore").hide();
-$("main").hide();
+// $("#underscore").hide();
+// $("main").hide();
 
-let i = 0;
-let siteOpen = "hello world";
-const speed = 60;
+// let i = 0;
+// let siteOpen = "hello world";
+// const speed = 60;
 
-function typing() {
-  if (i < siteOpen.length) {
-    document.getElementById("hello-world").innerHTML += siteOpen.charAt(i);
-    i++;
-    setTimeout(typing, speed);
-  };
-};
+// function typing() {
+//   if (i < siteOpen.length) {
+//     document.getElementById("hello-world").innerHTML += siteOpen.charAt(i);
+//     i++;
+//     setTimeout(typing, speed);
+//   };
+// };
 
-typing();
-$("#underscore").delay(700).fadeIn(300).fadeOut(300);
-$("#underscore").delay(500).fadeIn(300).fadeOut(300);
-$("#underscore").delay(500).fadeIn(300).fadeOut(300);
-$(".site-open").delay(3400).fadeOut(300);
+// typing();
+// $("#underscore").delay(700).fadeIn(300).fadeOut(300);
+// $("#underscore").delay(500).fadeIn(300).fadeOut(300);
+// $("#underscore").delay(500).fadeIn(300).fadeOut(300);
+// $(".site-open").delay(3400).fadeOut(300);
 
-$("main").delay(3800).fadeIn(800);
+// $("main").delay(3800).fadeIn(800);
 
 
 
@@ -31,7 +31,7 @@ $(".experience-contents").hide();
 
 $(".experience-tab").on("click", function () {
   $(".about").fadeOut(600);
-  $(".projects-page").fadeOut(600);
+  $(".work-page").fadeOut(600);
   $(".contact-page").fadeOut(600);
   $(".experience-page").animate({ width: "75vw" }, 700);
   $(".experience-contents").delay(200).fadeIn(500);
@@ -47,35 +47,35 @@ $(".experience-home-tab").on("click", function () {
   $(".experience-home-tab").delay(300).fadeOut(300);
   $(".experience-tab").delay(300).fadeIn(300);
   $(".contact-page").delay(400).fadeIn(700);
-  $(".projects-page").delay(300).fadeIn(700);
+  $(".work-page").delay(300).fadeIn(700);
   $(".about").delay(300).fadeIn(700);
 });
 
 
 
 
-$(".projects-page").css({ "height": "60px" });
-$(".projects-home-snippet").hide();
-$(".projects-home-tab").hide();
-$(".projects-contents").hide();
+$(".work-page").css({ "height": "60px" });
+$(".work-home-snippet").hide();
+$(".work-home-tab").hide();
+$(".work-contents").hide();
 
-$(".projects-tab").on("click", function () {
+$(".work-tab").on("click", function () {
   $(".about").fadeOut(500);
   $(".experience-page").fadeOut(500);
   $(".contact-page").fadeOut(500);
-  $(".projects-page").animate({ height: "80vh" }, 700);
-  $(".projects-contents").delay(200).fadeIn(500);
-  $(".projects-tab").delay(300).fadeOut(300);
-  $(".projects-home-tab").delay(300).fadeIn(300);
-  $(".projects-home-snippet").delay(400).fadeIn(500);
+  $(".work-page").animate({ height: "80vh" }, 700);
+  $(".work-contents").delay(200).fadeIn(500);
+  $(".work-tab").delay(300).fadeOut(300);
+  $(".work-home-tab").delay(300).fadeIn(300);
+  $(".work-home-snippet").delay(400).fadeIn(500);
 });
 
-$(".projects-home-tab").on("click", function () {
-  $(".projects-home-snippet").delay(100).fadeOut(600);
-  $(".projects-contents").fadeOut(700);
-  $(".projects-page").animate({ height: "60px" }, 700);
-  $(".projects-home-tab").delay(300).fadeOut(300);
-  $(".projects-tab").delay(300).fadeIn(300);
+$(".work-home-tab").on("click", function () {
+  $(".work-home-snippet").delay(100).fadeOut(600);
+  $(".work-contents").fadeOut(700);
+  $(".work-page").animate({ height: "60px" }, 700);
+  $(".work-home-tab").delay(300).fadeOut(300);
+  $(".work-tab").delay(300).fadeIn(300);
   $(".contact-page").delay(400).fadeIn(700);
   $(".experience-page").delay(400).fadeIn(700);
   $(".about").delay(400).fadeIn(700);
@@ -91,7 +91,7 @@ $(".contact-contents").hide();
 $(".contact-tab").on("click", function () {
   $(".about").fadeOut(600);
   $(".experience-page").fadeOut(500);
-  $(".projects-page").fadeOut(600);
+  $(".work-page").fadeOut(600);
   $(".contact-page").animate({ width: "75vw" }, 700);
   $(".contact-contents").delay(200).fadeIn(500);
   $(".contact-tab").delay(300).fadeOut(300);
@@ -106,6 +106,6 @@ $(".contact-home-tab").on("click", function () {
   $(".contact-home-tab").delay(300).fadeOut(300);
   $(".contact-tab").delay(300).fadeIn(300);
   $(".experience-page").delay(400).fadeIn(700);
-  $(".projects-page").delay(300).fadeIn(700);
+  $(".work-page").delay(300).fadeIn(700);
   $(".about").delay(300).fadeIn(700);
 });
